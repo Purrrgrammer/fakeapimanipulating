a48d956c2d5e3decbca00e596081a2049dbbfc15
John Vick <woram.j.vic@gmail.com>
2023-07-25 19:19:41 +0700

    still need checking after the latest commit in dev branch

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,14 +1,15 @@
 import { Post, Comment, CommentResponse } from "@/interface/placeHolder";
 import { postDetailService } from "@/service/detailService";
 import { useEffect, useState } from "react";
-import { Button, Card } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { Button, Card, InputGroup } from "react-bootstrap";
+import { Form, Link, useParams } from "react-router-dom";
+
 
 
 
 export const DetailPage = () => {
     const { id } = useParams()
-    const { postId } = useParams()
+    // const { postId } = useParams()
     //default
     const [postDetail, setPostDetail] = useState<Post>({
         userId: 0,
@@ -24,6 +25,7 @@ export const DetailPage = () => {
         body: 'fetch failed',
     })
     const [allComment, setAllComment] = useState<Comment[] | undefined>([])
+    const [commentStatus, setCommentStatus] = useState(false)
     //calling a post
 
     const callData = async (id: string) => {
@@ -33,7 +35,7 @@ export const DetailPage = () => {
         }
         try {
             if (response.data) {
-                setPostDetail(response.data)
+                setPostDetail(response.data) //set to a post
             }
             return response
         } catch (error) {
@@ -41,14 +43,15 @@ export const DetailPage = () => {
         }
     }
 
-    const callComments = async (id: string, postId: number) => {
-        const response = await postDetailService().getComment(id, postId)
+    const callComments = async (id: string) => {
+        const response = await postDetailService().getComment(id)
         if (response.status === 200) {
             console.log('comment data', response.data)
         }
         try {
             if (response.data) {
                 setAllComment(response.data)
+                setCommentStatus(true)
                 // console.log("recent comment", comment)
                 console.log("All comment", allComment)
             }
@@ -60,16 +63,14 @@ export const DetailPage = () => {
 
     useEffect(() => {
         if (id) callData(id || "1")
-        console.log("id", id)
-        console.log("postId", postId)
-        if (id || postId) callComments(id || "1", postId || 1)
-        console.log('post generated', id, postId)
-    }, [id, postId])
+        if (id) callComments(id || "1")
+        console.log('post and comment generated', id)
+    }, [id])
 
     return (
         <>
             {/* mapping data */}
-            <div className="bg-primary mt-100">
+            <div className="bg-light mt-100">
                 <Card className="mx-auto" style={{ height: "100%", width: "70%", padding: "20px" }}>
                     Post #{id}
                     <h1>{postDetail.userId}</h1>
@@ -82,10 +83,33 @@ export const DetailPage = () => {
                     <h3>share</h3>
                 </div>
                 {/* ---Comment Section=== */}
-                <div className="comment-section d-flex d-flex justify-content-around mx-5">
-                    <h1>{comment.email}</h1>
-                </div>
 
+                {commentStatus && (
+                    <div className="comment-section mx-5">
+                        {allComment?.map(el =>
+                            <div key={el.id}>
+                                <b>{el.name} : <span>{el.email}</span></b>
+                                <p key={el.id}>{el.body}</p>
+                            </div>)}
+
+
+                        <InputGroup className="mb-3">
+                            <input className="form-control"
+                                placeholder="say anything..."
+                                aria-label="say anything..."
+                                aria-describedby="basic-addon2"
+                            />
+                            <Button variant="outline-secondary" id="button-addon2">
+                                comment
+                            </Button>
+                        </InputGroup>
+
+
+
+
+                    </div>)
+
+                }
                 <Link to={`/`} className="text-decoration-none" >
                     <Button variant="dark" style={{ marginTop: '20px', marginLeft: '80%' }} className="">get back</Button>
                 </Link>
--- src/service/detailService.tsx
+++ src/service/detailService.tsx
@@ -6,8 +6,7 @@ import { URL } from "@/utillity/constant"
 
 export const postDetailService = () => {
     return {
-        getDetail: async (id: string
-        ): Promise<JResponse> => {
+        getDetail: async (id: string): Promise<JResponse> => {
             try {
                 const response = await axios.get(`${URL}/${id}`)
                 return handleResponse.success(response);
@@ -15,10 +14,9 @@ export const postDetailService = () => {
                 return error
             };
         },
-        getComment: async (id: string, postId: number
-        ): Promise<CommentResponse> => {
+        getComment: async (id: string): Promise<CommentResponse> => {
             try {
-                const response = await axios.get(`${URL}/${id}/comments?postId=${postId}`)
+                const response = await axios.get(`${URL}/${id}/comments`)
                 console.log("postId comment response", response)
                 return handleResponse.success(response);
             } catch (error: any) {
